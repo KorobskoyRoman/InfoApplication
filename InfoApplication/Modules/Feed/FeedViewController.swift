cbc9d8a6b7379bb1b1da3900ab9e964f2b382a56
Korobskoy Roman <korobskoyra@gmail.com>
2022-05-10 17:27:19 +0300

    Fixed feed frame

--- InfoApplication/Modules/Feed/FeedViewController.swift
+++ InfoApplication/Modules/Feed/FeedViewController.swift
@@ -36,7 +36,7 @@ final class FeedViewController: UIViewController {
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        self.collectionView.frame = self.view.frame
+        self.collectionView.frame = self.view.bounds
     }
     
 	override func viewDidLoad() {
