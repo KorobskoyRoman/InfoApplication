af8804984b319932b3fb62c56f10c8aee7fe2cd0
Korobskoy Roman <korobskoyra@gmail.com>
2022-05-10 17:27:02 +0300

    Added localization file

---
+++ InfoApplication/Helpers/Localization.swift
@@ -0,0 +1,16 @@
+//
+//  Localization.swift
+//  InfoApplication
+//
+//  Created by Roman Korobskoy on 10.05.2022.
+//
+
+import Foundation
+
+enum Localization {
+    static let feed = "Лента"
+    static let prize = "Конкурсы"
+    static let video = "Видео"
+    static let thumbsUp = "Годнота"
+    static let podcasts = "Подкасты"
+}
